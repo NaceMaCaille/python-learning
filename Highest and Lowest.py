302ce4b6e3f7e72e8735a7d5e6d033c201c41562
NaceMaCaille <ap620440@gmai.com>
2025-04-16 21:23:43 +0300

    Update Highest and Lowest.py

--- Highest and Lowest.py
+++ Highest and Lowest.py
@@ -1,4 +1,6 @@
 def high_and_low(numbers):
-    return ",".join(numbers)
-
-print(high_and_low("0 1 2 4 5 4 "))
+    maximum = str(max(int(elm) for elm in numbers.split())) 
+    minimum = str(min(int(elm) for elm in numbers.split()))
+    sum_str = maximum + " " + minimum
+    return sum_str
+print(high_and_low("8 3 -5 42 -1 0 0 -9 4 7 4 -4"))
