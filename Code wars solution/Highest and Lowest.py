084648d34118e0432a206b317bc5862c8d1fd54c
NaceMaCaille <ap620440@gmai.com>
2025-04-17 21:59:40 +0300

    Create Highest and Lowest.py

---
+++ Code wars solution/Highest and Lowest.py
@@ -0,0 +1,25 @@
+def high_and_low(numbers):
+    maximum = str(max(int(elm) for elm in numbers.split())) 
+    minimum = str(min(int(elm) for elm in numbers.split()))
+    sum_str = maximum + " " + minimum
+    return sum_str
+print(high_and_low("8 3 -5 42 -1 0 0 -9 4 7 4 -4"))
+
+def high_and_low_variant1(numbers):
+    n = [int(elm) for elm in numbers.split(" ")]
+    return f"{max(n)} {min(n)}"
+print(high_and_low_variant1("8 3 -5 42 -1 0 0 -9 4 7 4 -4"))
+
+def high_and_low_variant2(numbers):
+    n = sorted(numbers.split(" "), key=int)
+    return "{} {}".format(n[-1], n[0])
+print(high_and_low_variant2("8 3 -5 42 -1 0 0 -9 4 7 4 -4"))
+
+def high_and_low_variant3(numbers):
+    n = list(map(int, numbers.split()))
+    return "{} {}".format(max(n), min(n))
+print(high_and_low_variant3("8 3 -5 42 -1 0 0 -9 4 7 4 -4"))
+
+def high_and_low_variant4(numbers):
+    return f"{max(map(int, numbers.split()))} {min(map(int, numbers.split()))}"
+print(high_and_low_variant4("8 3 -5 42 -1 0 0 -9 4 7 4 -4"))
