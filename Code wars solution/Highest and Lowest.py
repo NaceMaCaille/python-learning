dbaedd3a5a204d9c2068fa211b2b1a8ea08e9924
NaceMaCaille <ap620440@gmai.com>
2025-04-18 12:28:39 +0300

    Update Highest and Lowest.py

--- Code wars solution/Highest and Lowest.py
+++ Code wars solution/Highest and Lowest.py
@@ -1,9 +1,9 @@
-def high_and_low(numbers):
+def high_and_low_variant0(numbers):
     maximum = str(max(int(elm) for elm in numbers.split())) 
     minimum = str(min(int(elm) for elm in numbers.split()))
     sum_str = maximum + " " + minimum
     return sum_str
-print(high_and_low("8 3 -5 42 -1 0 0 -9 4 7 4 -4"))
+print(high_and_low_variant0("8 3 -5 42 -1 0 0 -9 4 7 4 -4"))
 
 def high_and_low_variant1(numbers):
     n = [int(elm) for elm in numbers.split(" ")]
