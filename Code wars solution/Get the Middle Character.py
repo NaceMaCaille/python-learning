62fd0f53bdae8bd6cb581edc8d60c2b3c8b903ec
NaceMaCaille <ap620440@gmai.com>
2025-04-18 14:05:43 +0300

    Update Get the Middle Character.py

--- Code wars solution/Get the Middle Character.py
+++ Code wars solution/Get the Middle Character.py
@@ -22,3 +22,26 @@ print(get_middle_variant1("apple"))
 
 #If the string length is even, it will return 2 middle characters.
 print(get_middle_variant1("banana"))
+
+def get_middle_variant2(s):
+    x = len(s)
+    y = int(x/2)
+    if x % 2 == 0:
+        return s[y -1: y + 1]
+    else:
+        return s[y:y + 1]
+print(get_middle_variant2("chery"))
+
+print(get_middle_variant2("pinapple"))
+
+def get_middle_variant3(s):
+    y = (len(s) - 1) // 2
+    return s[y: -y] or s
+print(get_middle_variant3("testing"))
+
+print(get_middle_variant3("ice"))
+
+def get_middle_variant4(s):
+    return s[(len(s) -1) // 2 : (len(s) + 2) // 2]
+print(get_middle_variant4("Opera"))
+print(get_middle_variant4("Google"))
