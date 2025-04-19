b95d63f6727bb95e8f592c9370b7fc1c7fa2b523
NaceMaCaille <ap620440@gmai.com>
2025-04-19 17:37:26 +0300

    Update Project API.py

--- Project/Project API.py
+++ Project/Project API.py
@@ -1,7 +1,5 @@
-CreateTodo = {'id': 1, 'action': 'купити продукти ', 'date': '18.04.2025.23:52'} 
-print(CreateTodo)
-
-
+#CreateTodo = {'id': 1, 'action': 'купити продукти ', 'date': '18.04.2025.23:52'} 
+#print(CreateTodo)
 
 def CreatrTodo(id, a, d):
     return {
@@ -10,9 +8,16 @@ def CreatrTodo(id, a, d):
         'date':d
     }
 
-Get_list = print(CreatrTodo(2,'посуд','19.04.2025')) 
-Get_list = print(CreatrTodo(3,'покормити кота','19.04.2025.12:00'))
+#Get_list = print(CreatrTodo(2,'посуд','19.04.2025')) 
+#Get_list = print(CreatrTodo(3,'покормити кота','19.04.2025.12:00'))
 
+s = input()
+
+if "Get list":
+    id = input('Id')
+    action = input("Action")
+    date = input("Date")
+    Get_list = print(CreatrTodo(id,action,date))
 
 
 
