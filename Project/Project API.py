8f1fe3fe4e8e0ee145a13802d987ae1cc6e2ab92
NaceMaCaille <ap620440@gmai.com>
2025-04-19 14:17:12 +0300

    Update Project API.py

--- Project/Project API.py
+++ Project/Project API.py
@@ -1,4 +1,12 @@
 def CreateTodo(action, date):
+    def EditTodo(action, date):
+        return "{} {}".format(action, date)
     Create = {f"id": "1", "action": f"{action}", "date": f"{date}"}
     return Create
+    
+        
+
+
+
 print(CreateTodo("купити продукти","18.04.2025.23:52"))
+print(EditTodo)
