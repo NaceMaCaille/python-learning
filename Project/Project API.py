fa044de94a978487776aa27a1fef318bc19442c7
NaceMaCaille <ap620440@gmai.com>
2025-04-19 14:21:10 +0300

    Update Project API.py

--- Project/Project API.py
+++ Project/Project API.py
@@ -1,12 +1,10 @@
 def CreateTodo(action, date):
-    def EditTodo(action, date):
-        return "{} {}".format(action, date)
-    Create = {f"id": "1", "action": f"{action}", "date": f"{date}"}
-    return Create
-    
-        
+    return
+
+library = {'id': 1, 'action': 'купити продукти ', 'date': '18.04.2025.23:52'} 
+library1 = library.copy()
+print(library1)
 
 
 
-print(CreateTodo("купити продукти","18.04.2025.23:52"))
-print(EditTodo)
+
