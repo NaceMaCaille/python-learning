39002ad7b664713a9da315feb536e610c00c781b
NaceMaCaille <ap620440@gmai.com>
2025-04-19 18:46:46 +0300

    Update Project API.py

--- Project/Project API.py
+++ Project/Project API.py
@@ -1,7 +1,7 @@
 #CreateTodo = {'id': 1, 'action': 'купити продукти ', 'date': '18.04.2025.23:52'} 
 #print(CreateTodo)
 
-def CreatrTodo(id, a, d):
+def CreateTodo(id, a, d):
     return {
         'id': id,
         'action':a,
@@ -17,9 +17,10 @@ if "Get list":
     num_of_reminders = input()
     for iteration in range(1,int(num_of_reminders)):
         id = iteration
-        action = input("Action - ")
-        date = input("Date - ")
-        Get_list = print(CreatrTodo(id,action,date))
+        action = input("Нагадування - ")
+        date = input("Дата - ")
+        Get_list = print(CreateTodo(id,action,date))
+    
 
 
 
