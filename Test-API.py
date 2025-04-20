4e0809f2e2d31d5e7134bd17bf1952667cde563c
NaceMaCaille <ap620440@gmai.com>
2025-04-20 14:18:57 +0300

    Update Test-API.py

--- Test-API.py
+++ Test-API.py
@@ -4,7 +4,7 @@ import pytz
 todo_list = []
 curent_id = 1
 
-def CreateTodo(action):
+def createTodo(action):
     time_zone = datetime.now(pytz.timezone('Europe/Kyiv'))
     global curent_id
     
@@ -16,11 +16,24 @@ def CreateTodo(action):
     todo_list.append(saved_list)
     curent_id += 1
 
+def editTodo(id,action):
+    for todo in todo_list:
+        if todo['id'] == id:
+            todo['action'] = action
+            break
+
+def removeTodo(id):
+    for todo in todo_list:
+        if todo['id'] == id:
+            todo.clear()
+
 def getTodolist():
-    return todo_list
-    
-CreateTodo('погладить кота')
-CreateTodo('приготувати їжу')
+    return todo_list 
+
+createTodo('погладить кота')
+createTodo('приготувати їжу')
+editTodo(1,'погладить собаку')
+removeTodo(2)
 
 for listTodo in getTodolist():
     print(listTodo)
