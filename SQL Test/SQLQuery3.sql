dc0cd7a0cabae7b9ff6c25d132a7fd0a71bb8a65
NaceMaCaille <ap620440@gmai.com>
2025-04-23 23:49:46 +0300

    Delete SQLQuery3.sql

--- SQL Test/SQLQuery3.sql
+++
@@ -1,9 +0,0 @@
--- create database turtle;
--- create table Orders(OrderId smallint, OrderName nvarchar(50));
-insert into Orders values(1,'A');
-insert into Orders values(2,'B');
-insert into Orders values(3,'C');
-insert into Orders values(4,'D');
-
-select * from Orders;
-select @@SERVERNAME
