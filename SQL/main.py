29aeb826903f5527a0a72ad081be5fa42e02a02c
NaceMaCaille <ap620440@gmai.com>
2025-04-24 00:22:51 +0300

    Update main.py

--- SQL/main.py
+++ SQL/main.py
@@ -1,11 +1,13 @@
 import sqlite3 as sq
  
-with sq.connect("Test.db") as con:
+with sq.connect("Testdb.db") as con:
     cur = con.cursor()
 
-    cur.execute(""" CREATE TABLE users(
-        name TEXT,
-        sex INTEGER,
+    cur.execute("DROP TABLE IF EXISTS uesrs")
+    cur.execute(""" CREATE TABLE IF NOT EXISTS users(
+        user_id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL,
+        sex INTEGER NOT NULL DEFAULT 1,
         old INTEGER,
         score INTEGER
         )""")
