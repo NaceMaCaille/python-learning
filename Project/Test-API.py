c080cc4df5d38dec829ed8168aec724d034e6e80
NaceMaCaille <ap620440@gmai.com>
2025-04-25 00:02:50 +0300

    Update Test-API.py

--- Project/Test-API.py
+++ Project/Test-API.py
@@ -8,6 +8,103 @@ category =  ['спорт', 'навчання', 'покупки', 'робота',
 time_zone = datetime.now(pytz.timezone('Europe/Kyiv'))
 formated = time_zone.strftime("%d.%m.%Y %H:%M:%S")
 
+def chooseOption():
+    def countTodo_inteface():
+        action = int(input("Кількість нагадувань - "))
+
+        print("1 - Cпорт")
+        print("2 - Навчання")
+        print("3 - Покупки")
+        print("4 - Робота")
+        print("5 - Без категорії")
+
+        select_category = int(input("Оберіть категорію нагадувань - "))
+
+        success = countTodo(action, select_category)
+
+        if success:
+            print("Статус нагадування оновлено.")
+        else:
+            print("Erorr")
+
+    def completeTodo_inteface(): #Complete
+        todo_id = int(input("Введіть ID нагадування для помітки - "))
+        print("1 - Так, виконав")
+        print("2 - Ні, не виконав")
+        choice = int(input("Ви виконали це нагадування - "))
+
+        completed = choice == 1
+
+        success = completeTodo(todo_list,todo_id,completed)
+        
+        if success:
+            print("Статус нагадування оновлено.")
+        else:
+            print("Нагадування з таким ID не знайдено.")
+        
+    def editTodo_inteface(): #Edit
+        id = int(input("Введіть ID для редагування - "))
+
+        action = input("Нове нагадування - ")
+            
+        print("1 - Cпорт")
+        print("2 - Навчання")
+        print("3 - Покупки")
+        print("4 - Робота")
+        print("5 - Без категорії")
+            
+        select_cat = int(input('Змініть категорію - '))
+
+        successEdit = editTodo(id,action,select_cat)
+        if successEdit:
+            print("Нагадування оновлено.")
+        else:
+            print("Нагадування з таким ID не знайдено.")
+
+    def removeTodo_inteface(): #Remove
+        id = int(input("Введіть ID нагадування для видалення - "))
+
+        successRemove = removeTodo(id)
+
+        if successRemove:
+            print("Нагадування видалено.")
+        else:
+            print("Erorr")
+    
+    option = {
+    '1':countTodo_inteface,
+    '2':editTodo_inteface,
+    '3':removeTodo_inteface,
+    '4':sortNameTodo,
+    '5':sortDateTodo,
+    '6':completeTodo_inteface,
+    '7':getTodolist,
+    '0':exit
+    }
+
+    
+    
+    while True:
+        print("Меню")
+        print("1 - Додати кілька нагадувань")
+        print("2 - Редагувати нагадування")
+        print("3 - Видалити нагадування")
+        print("4 - Сортувати за назвою")
+        print("5 - Сортувати за датою")
+        print("6 - Помітити нагадування")
+        print("7 - Показати всі нагадування")
+        print("0 - Вихід")
+    
+        select_option = input("Оберіть опцію - ")
+
+
+        command = option.get(select_option)
+        if command:
+            command()
+        else:
+            print("Erorr")
+        
+
 def createTodo(action, cat):
     global curent_id
     
@@ -21,53 +118,23 @@ def createTodo(action, cat):
     todo_list.append(saved_list)
     curent_id += 1
 
-def editTodo(): # Edit
-    
-    id = int(input("Введіть ID нагадування для редагування - "))
-    
-    action = input("Нове нагадування - ")
+def editTodo(id,action,select_cat): # Edit
     for todo in todo_list:
         if todo['id'] == id:
             todo['action'] = action
             todo.update({'editDate': formated})
             
-            print("1 - Cпорт")
-            print("2 - Навчання")
-            print("3 - Покупки")
-            print("4 - Робота")
-            print("5 - Без категорії")
-
-            select_cat = int(input('Змініть категорію - '))
             if 1 <= select_cat <= 5:
                 todo.update({'category':category[select_cat - 1]})
                 break
 
-def removeTodo(): # Clear
-    id = int(input("Введіть ID нагадування для видалення - "))
+def removeTodo(id): # Clear
     for todo in todo_list:
         if todo['id'] == id:
             todo_list.remove(todo)
 
-def countTodo(): # Count
-    
-    try:
-        action = int(input("Кількість нагадувань - "))
-    except ValueError:
-        print("Потрібно ввести число")
-        return
-    
-    print("1 - Cпорт")
-    print("2 - Навчання")
-    print("3 - Покупки")
-    print("4 - Робота")
-    print("5 - Без категорії")
-    
-    try:
-        select_category = int(input("Оберіть категорію нагадувань - "))
-    except ValueError:
-            print("Оберіть категорію")
-            return
-    
+def countTodo(action,select_category): # Count
+
     if 1 <= select_category <= 5:
         select_cat = category[select_category - 1]
     else:
@@ -77,19 +144,13 @@ def countTodo(): # Count
         it = input("Нагадування - ")
         createTodo(it,select_cat)
 
-def completeTodo(): # Complete todo
-    id = int(input("Введіть ID нагадування для помітки - "))
-    for is_complete in todo_list:
-        if is_complete['id'] == id:
-            print("1 - Так виконав")
-            print("2 - Не виконав")
-            complete = int(input("Ви виконали це нагадування - "))
-        
-            if complete == 1:
-                is_complete.update({"isComplete":True})
-
-            elif complete == 2:
-                break
+def completeTodo(todo_list,id,complete): # Complete todo
+   
+    for todo in todo_list:
+        if todo['id'] == id:
+            todo['isComplete'] = complete
+            return True
+        return False
             
 def sortNameTodo(): # Sort 
     sorted_name = sorted(todo_list, key=lambda name: name['action'])
@@ -107,34 +168,6 @@ def getTodolist(): # Get
         print(listTodo)
     
     
-option = {
-    '1':countTodo,
-    '2':editTodo,
-    '3':removeTodo,
-    '4':sortNameTodo,
-    '5':sortDateTodo,
-    '6':completeTodo,
-    '7':getTodolist,
-    '0':exit
-}
+chooseOption()
 
-while True:
-    print("Меню")
-    print("1 - Додати кілька нагадувань")
-    print("2 - Редагувати нагадування")
-    print("3 - Видалити нагадування")
-    print("4 - Сортувати за назвою")
-    print("5 - Сортувати за датою")
-    print("6 - Помітити нагадування")
-    print("7 - Показати всі нагадування")
-    print("0 - Вихід")
-
-
-    select_option = input("Оберіть опцію - ")
-
-    command = option.get(select_option)
-    if command:
-        command()
-    else:
-        print("Erorr")
 
