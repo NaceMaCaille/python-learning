2fc733bbcc91e80bad9f421880084993e9145ba1
NaceMaCaille <ap620440@gmai.com>
2025-04-25 00:04:41 +0300

    Update main.py

--- SQL/main.py
+++ SQL/main.py
@@ -3,8 +3,8 @@ import sqlite3 as sq
 with sq.connect("Testdb.db") as con:
     cur = con.cursor()
 
-    cur.execute("""CREATE TABLE 'games'(
-        'user_id' INTEGER,
-        'score' INTEGER,        
-        'time' INTEGER
-        )""");
+    cur.execute ("""CREATE TABLE "games" (
+	    "Field1"	INTEGER,
+	    "Field2"	INTEGER,
+	    "Field3"	INTEGER
+        """);
