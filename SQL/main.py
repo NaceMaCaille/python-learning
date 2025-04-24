a8766eefbdea1aac3bf47cb72c5a4231b2533664
NaceMaCaille <ap620440@gmai.com>
2025-04-24 19:08:21 +0300

    Update main.py

--- SQL/main.py
+++ SQL/main.py
@@ -3,6 +3,8 @@ import sqlite3 as sq
 with sq.connect("Testdb.db") as con:
     cur = con.cursor()
 
-    cur.execute("SELECT * FROM users WHERE score > 100 ORDER BY score DESC LIMIT 5")
-    result = cur.fetchall()
-    print(result)
+    cur.execute("""CREATE TABLE 'games'(
+        'user_id' INTEGER,
+        'score' INTEGER,        
+        'time' INTEGER
+        )""");
