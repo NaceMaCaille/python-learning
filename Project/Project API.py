ebedf7e01efb0a278a2f761639a433e3c33105c6
NaceMaCaille <ap620440@gmai.com>
2025-04-24 19:09:59 +0300

    Update Project API.py

--- Project/Project API.py
+++ Project/Project API.py
@@ -3,23 +3,26 @@ import pytz
 
 todo_list = []
 curent_id = 1
+category =  ['спорт', 'навчання', 'покупки', 'робота',None]
 
 time_zone = datetime.now(pytz.timezone('Europe/Kyiv'))
 formated = time_zone.strftime("%d.%m.%Y %H:%M:%S")
 
-def createTodo(action):
+def createTodo(action, cat):
     global curent_id
     
     saved_list = {
         'id': curent_id,
         'action':action,
         'date':formated,
+        'category':cat,
         'isComplete':False
             }
     todo_list.append(saved_list)
     curent_id += 1
 
-def editTodo():
+def editTodo(): # Edit
+    
     id = int(input("Введіть ID нагадування для редагування - "))
     
     action = input("Нове нагадування - ")
@@ -27,21 +30,54 @@ def editTodo():
         if todo['id'] == id:
             todo['action'] = action
             todo.update({'editDate': formated})
-            break
+            
+            print("1 - Cпорт")
+            print("2 - Навчання")
+            print("3 - Покупки")
+            print("4 - Робота")
+            print("5 - Без категорії")
 
-def removeTodo():
+            select_cat = int(input('Змініть категорію - '))
+            if 1 <= select_cat <= 5:
+                todo.update({'category':category[select_cat - 1]})
+                break
+
+def removeTodo(): # Clear
     id = int(input("Введіть ID нагадування для видалення - "))
     for todo in todo_list:
         if todo['id'] == id:
-            todo.clear()
+            todo_list.remove(todo)
 
-def countTodo():
-    action = int(input("Кількість нагадувань - "))
+def countTodo(): # Count
+    
+    try:
+        action = int(input("Кількість нагадувань - "))
+    except ValueError:
+        print("Потрібно ввести число")
+        return
+    
+    print("1 - Cпорт")
+    print("2 - Навчання")
+    print("3 - Покупки")
+    print("4 - Робота")
+    print("5 - Без категорії")
+    
+    try:
+        select_category = int(input("Оберіть категорію нагадувань - "))
+    except ValueError:
+            print("Оберіть категорію")
+            return
+    
+    if 1 <= select_category <= 5:
+        select_cat = category[select_category - 1]
+    else:
+        print("Категорію не обрано")
+    
     for _ in range(action):
         it = input("Нагадування - ")
-        createTodo(it)
+        createTodo(it,select_cat)
 
-def completeTodo():
+def completeTodo(): # Complete todo
     id = int(input("Введіть ID нагадування для помітки - "))
     for is_complete in todo_list:
         if is_complete['id'] == id:
@@ -51,22 +87,22 @@ def completeTodo():
         
             if complete == 1:
                 is_complete.update({"isComplete":True})
-                
+
             elif complete == 2:
                 break
             
-def sortNameTodo():
+def sortNameTodo(): # Sort 
     sorted_name = sorted(todo_list, key=lambda name: name['action'])
     for sort_list_todo in sorted_name:
         print(sort_list_todo)
 
-def sortDateTodo():
+def sortDateTodo(): # Sort
     sorted_date = sorted(todo_list, key=lambda date: date['date'],reverse=True)
     for sort_list_todo in sorted_date:
         print(sort_list_todo)
 
 
-def getTodolist():
+def getTodolist(): # Get
     for listTodo in todo_list:
         print(listTodo)
     
