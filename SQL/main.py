de6ab6ac31666c4a804914b326bff882bbd86116
NaceMaCaille <ap620440@gmai.com>
2025-04-26 01:17:38 +0300

    Update main.py

--- SQL/main.py
+++ SQL/main.py
@@ -1,10 +1,33 @@
 import sqlite3 as sq
- 
-with sq.connect("Testdb.db") as con:
+
+cars = [ 
+    ('Audi', 52642), 
+    ('Mercedes', 57127), 
+    ('Skoda', 9000), 
+    ('Volvo', 29000), 
+    ('Bentley', 350000) 
+]
+con = None
+
+try:
+    con = sq.connect("cars.db")
     cur = con.cursor()
 
-    cur.execute ("""CREATE TABLE "games" (
-	    "Field1"	INTEGER,
-	    "Field2"	INTEGER,
-	    "Field3"	INTEGER
-        """);
+    cur.execute("""CREATE TABLE IF NOT EXISTS cars (
+        car_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        model TEXT,
+        price INTEGER
+    );""")
+
+    cur.executemany("INSERT INTO cars VALUES (NULL, ?, ?)", cars)
+
+    cur.execute("UPDATE cars SET price = price + 1000")
+    
+    con.commit()
+
+except sq.Error as e:
+    if con: con.rollback()
+    print("Erorr")
+finally:
+    if con: con.close()
+
