45c3d9a6b6ad9fca0d5e7e78b3f79f2eb20ee64d
NaceMaCaille <ap620440@gmai.com>
2025-04-26 17:39:23 +0300

    Update Project API.py

--- Project/Project API.py
+++ Project/Project API.py
@@ -1,8 +1,131 @@
 from datetime import datetime
 import pytz
+import sqlite3
 
-todo_list = []
-curent_id = 1
+con = sqlite3.connect('API Database.db')
+cur = con.cursor()
+
+cur.execute(""" 
+CREATE TABLE IF NOT EXISTS categories (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT UNIQUE
+)
+""")
+
+cur.execute(""" 
+CREATE TABLE IF NOT EXISTS todos (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    action TEXT NOT NULL,
+    date_created TEXT NOT NULL,
+    edit_date TEXT,
+    is_complete INTEGER DEFAULT 0,
+    category_id INTEGER,
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+)
+""")
+category_list =  ['спорт', 'навчання', 'покупки', 'робота',None]
+for cat in category_list:
+    if cat is not None:
+        cur.execute("INSERT OR IGNORE INTO categories (name) VALUES (?)",(cat,))
+con.commit()
+
+
+def createTodo(action, cat):
+    date = formated
+    
+    if 1 <= cat <= 5:
+        cat_name = category_list[cat - 1]
+        if cat_name:
+            cur.execute("SELECT id FROM categories WHERE name = ?", (cat_name,))
+            category = cur.fetchone()
+            category_id = category[0] if category else None 
+        else:
+            category_id = None
+    else:
+        category_id = None
+
+    cur.execute(""" 
+    INSERT INTO todo (action, data_created, category_id)
+    VALUES (?,?,?)
+    """, (action,date,category_id)) 
+    con.commit()
+
+def editTodo(id,action,select_cat): # Edit
+    date = formated
+    
+    if 1 <= cat <= 5:
+        cat_name = category_list[cat - 1]
+        if cat_name:
+            cur.execute("SELECT id FROM categories WHERE name = ?", (cat_name,))
+            category = cur.fetchone()
+            category_id = category[0] if category else None 
+        else:
+            category_id = None
+    else:
+        category_id = None
+
+    cur.execute(""" 
+    UPDATE todo
+    SET action = ? edit_date = ?, category_id =?
+    WHERE id = ?
+    """, (action,date,category_id,id)) 
+    con.commit()
+
+    return cur.rowcount > 0
+
+def removeTodo(id): # Clear
+    cur.execute('DELETE FROM todo WHERE id = ?', (id,))
+    con.commit()
+    return cur.rowcount > 0
+
+def countTodo(action,select_category): # Count
+
+    if 1 <= select_category <= 5:
+        select_cat = category[select_category - 1]
+    else:
+        print("Категорію не обрано")
+    
+    for _ in range(action):
+        it = input("Нагадування - ")
+        createTodo(it,select_cat)
+
+def completeTodo(todo_list,id,complete): # Complete todo
+    cur.execute(""" 
+    UPDATE todo
+    SET is_complete = ?
+    WHERE id = ?
+    """, (int(complete),id))
+    con.commit()
+    return cur.rowcount > 0
+            
+def sortNameTodo(): # Sort 
+    cur.execute("""
+    SELECT todo.id, todo.action, todo.date_created, todo.is_complete, categories.name
+    FROM todo
+    LEFT JOIN catgories ON todo.category_id = categories.id
+    ORDER BY todo.action
+    """)
+    for sort_n in cur.fetchall():
+        print(sort_n)
+
+
+def sortDateTodo(): # Sort
+    cur.execute("""
+    SELECT todo.id, todo.action, todo.date_created, todo.is_complete, categories.name
+    FROM todo
+    LEFT JOIN catgories ON todo.category_id = categories.id
+    ORDER BY todo.date_created DESC
+    """)
+    for sort_d in cur.fetchall():
+        print(sort_d)
+
+def getTodolist(): # Get
+    cur.execute(""" 
+    SELECT todo.id, todo.action, todo.date_created, todo.is_complete, categories.name
+    FROM todo 
+    LEFT JOIN categories ON todo.category_id = category_id
+    """)
+
 category =  ['спорт', 'навчання', 'покупки', 'робота',None]
 
 time_zone = datetime.now(pytz.timezone('Europe/Kyiv'))
@@ -35,7 +158,7 @@ def chooseOption():
 
         completed = choice == 1
 
-        success = completeTodo(todo_list,todo_id,completed)
+        success = completeTodo(None,todo_id,completed)
         
         if success:
             print("Статус нагадування оновлено.")
@@ -115,71 +238,7 @@ def chooseOption():
             command()
         else:
             print("Erorr")
-        
-
-def createTodo(action, cat):
-    global curent_id
-    
-    saved_list = {
-        'id': curent_id,
-        'action':action,
-        'date':formated,
-        'category':cat,
-        'isComplete':False
-            }
-    todo_list.append(saved_list)
-    curent_id += 1
-
-def editTodo(id,action,select_cat): # Edit
-    for todo in todo_list:
-        if todo['id'] == id:
-            todo['action'] = action
-            todo.update({'editDate': formated})
             
-            if 1 <= select_cat <= 5:
-                todo.update({'category':category[select_cat - 1]})
-                break
-
-def removeTodo(id): # Clear
-    for todo in todo_list:
-        if todo['id'] == id:
-            todo_list.remove(todo)
-
-def countTodo(action,select_category): # Count
-
-    if 1 <= select_category <= 5:
-        select_cat = category[select_category - 1]
-    else:
-        print("Категорію не обрано")
-    
-    for _ in range(action):
-        it = input("Нагадування - ")
-        createTodo(it,select_cat)
-
-def completeTodo(todo_list,id,complete): # Complete todo
-   
-    for todo in todo_list:
-        if todo['id'] == id:
-            todo['isComplete'] = complete
-            return True
-        return False
-            
-def sortNameTodo(): # Sort 
-    sorted_name = sorted(todo_list, key=lambda name: name['action'])
-    for sort_list_todo in sorted_name:
-        print(sort_list_todo)
-
-def sortDateTodo(): # Sort
-    sorted_date = sorted(todo_list, key=lambda date: date['date'],reverse=True)
-    for sort_list_todo in sorted_date:
-        print(sort_list_todo)
-
-
-def getTodolist(): # Get
-    for listTodo in todo_list:
-        print(listTodo)
-    
-    
 chooseOption()
 
-
+con.close()
