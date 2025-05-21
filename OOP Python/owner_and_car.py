c6a104e230eb5c56416de74a963c18c947a80ff4
NaceMaCaille <ap620440@gmail.com>
2025-05-21 21:36:25 +0300

    revieved change in owner and car

--- OOP Python/owner_and_car.py
+++ OOP Python/owner_and_car.py
@@ -18,7 +18,7 @@ class Car:
     
     
     def set_owner(self, owner):
-        if owner.age > 18:
+        if owner.age >= 18:
             self.owner = owner
         else:
             raise ValueError('Помилка! Власник неповнолітній')
@@ -28,18 +28,22 @@ class Car:
         return (
             f"Бренд - {self.brand}, Модель - {self.model},"
             f"Рік створення - {self.year_creating}, Номерний знак - {self.number_plate}"
-            f"\n{self.owner.name} {self.owner.age}"
+            f"\nІм'я - {self.owner.name}, Прізвище - {self.owner.age}"
             )
         
             
     def __repr__(self):
         return self.get_info()
     
-try:
-    person = Human('Albert', 17)
-    car = Car('Bugati','Bolide',2020,'CB 0001 AA')
-    car.set_owner(person)
+    
+person = Human('Albert', 18)
+car = Car('Bugati','Bolide',2020,'CB 0001 AA')
 
+
+try:
+    car.set_owner(person)
     print(car)
 except ValueError as e:
     print(e)
+    
+print(car.get_info())
