c2bf33ab4be502dc9417eb9102f895769a11ca7d
NaceMaCaille <ap620440@gmail.com>
2025-05-24 02:42:03 +0300

    add method in class Teacher

--- OOP Python/teacher_and_student.py
+++ OOP Python/teacher_and_student.py
@@ -6,7 +6,7 @@ class Human:
     
     
     def get_full_name(self):
-        full_name = f"Ім'я - {self.name}, Прізвище - {self.surname}"
+        full_name = f"{self.name} {self.surname}"
         return full_name
     
     
@@ -25,24 +25,52 @@ class Student(Human):
         self.marks = marks
         
     def get_average_mark(self):
-        return (f"{self.get_full_name}\n"
-        f"Середня оцінка - {sum(self.marks) // len(self.marks)}")
+        return sum(self.marks) // len(self.marks)
     
     
     def get_min_mark(self):    
-        return f"{self.get_full_name}\nМінімальна оцінка - {min(self.marks)}" 
+        return min(self.marks)
     
     
     def get_max_mark(self):    
-        return f"{self.get_full_name()}\nМаксимальна оцінка - {max(self.marks)}"
-    
-    
+        return max(self.marks)
 
-          
-    
-male_student = Student("Іван", "Петренко", 30,[10,9,8,1,10])    
-print(male_student.get_average_mark())
-print(male_student.get_min_mark())
-print(male_student.get_max_mark())
+
+class Teacher:
+    def __init__(self,students):
+        self.students = students
+
+
+    def get_list_of_names_by_average_mark(self):
+        return (sorted(self.students,
+                       key=lambda student: student.get_average_mark(),
+                       reverse=True))
+
+
+    def get_student_by_name(self, name):
+        pass
+
+
+    def remove_student_by_name(self, name):
+        pass
+
+
+ivan = Student("Іван", "Петренко", 24,[4, 8, 3, 2, 1])
+albert = Student("Альберт", "Мішустін", 25, [5, 10, 11, 3, 1])
+irina = Student("Ірина", "Шевченко", 21, [10, 1, 4, 1, 7])
+daria = Student("Дар'я", "Коваленко", 23, [1, 10, 1, 3, 6])
+
+student_group = [ivan, albert, irina, daria]
+
+lidia = Teacher(student_group)
+print(lidia.get_list_of_names_by_average_mark())
+
+print(
+     f"Студент {ivan.get_full_name()}",
+     "має середню оцінку",
+     ivan.get_average_mark()
+   )
+
+
 
 
